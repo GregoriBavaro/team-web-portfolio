077fef4ac291b2c1be81553fce60021a7ea497c4
Gregori <greg.gego@gmail.com>
2023-05-03 13:45:30 +0200

    stars-anime

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,13 +2,21 @@
 import HeroText from "../components/HeroText";
 import LayoutWrapper from "../UI/LayoutWrapper";
 
-
 const Home = () => {
   return (
     <LayoutWrapper>
-      <div id="stars"></div>
-      <div id="stars2"></div>
-      <div id="stars3"></div>
+      <div
+        style={{
+          position: "absolute",
+          width: "100%",
+          height: "100%",
+          left: "-10%",
+        }}
+      >
+        <div id="stars"></div>
+        <div id="stars2"></div>
+        <div id="stars3"></div>
+      </div>
       <HeroText />
     </LayoutWrapper>
   );
